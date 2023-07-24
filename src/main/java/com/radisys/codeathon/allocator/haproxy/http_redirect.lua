f4493fd690beb8a37af9e986798673811e06ee91
raukumar <raukumar@radisys.com>
2023-07-24 17:33:49 +0530

    adding updated HAProxy files

--- src/main/java/com/radisys/codeathon/allocator/haproxy/http_redirect.lua
+++ src/main/java/com/radisys/codeathon/allocator/haproxy/http_redirect.lua
@@ -1,11 +1,5 @@
 lunajson = require 'lunajson'
 
-local function hget_nems(id,hash_key)
-    local redis = (require 'redis').connect('127.0.0.1', 6379)
-    local flat_map = redis:hget(hash_key,id)
-    local result = lunajson.decode(flat_map)
-    return result.negId
-end
 
 local function hget_current(id,hash_key)
     local redis = (require 'redis').connect('127.0.0.1', 6379)
@@ -14,10 +8,17 @@ local function hget_current(id,hash_key)
     return result.currentAllocation
 end
 
+function get_token(txn)
+   local http = require("socket.http")
+   response, err = http.request("POST", "http://idp.ems602.dtblrrsys.com/auth/realms/a4/protocol/openid-connect/token")
+
+end
+core.register_fetches("get_token", get_token)
+
 
 function get_currentallocation(txn)
   local redis = (require 'redis').connect('127.0.0.1', 6379)
-  local value=txn:get_var("txn.value")
+  local value=txn.sf:hdr("neg")
   if value == 'NEG-602' then
     result = hget_current('NEG-602','NEMS_NEG_MAPPING')
     print("The request is forwarded to ".. result)
@@ -29,19 +30,3 @@ function get_currentallocation(txn)
 end
 
 core.register_fetches("get_currentallocation", get_currentallocation)
-
-function get_backend(txn)
-  local redis = (require 'redis').connect('127.0.0.1', 6379)
-  local path = txn.sf:hdr('neg')
-  local path_start = string.match(path, '([^/]+)')
-  if path_start == 'NEG-602' then
-    result = hget_nems('NEG-602','NEMS_NEG_MAPPING')
-    print("The request is routed to the backend server ".. result)
-  elseif path_start == 'NEG-1602' then
-    result = hget_nems('NEG-1602','NEMS_NEG_MAPPING')
-    print("The request is routed to the backend server ".. result)
-  end
-  return result
-end
-
-core.register_fetches("get_backend", get_backend)
